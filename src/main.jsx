0974e5830b28b5657ef8a1b8c32d82ca7512a5a0
SaiTeja-2101 <akulasaiteja2006@gmail.com>
2025-06-20 02:33:12 +0530

    Updated  main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/movie-list-app/">
     <App />
   </BrowserRouter>,
 )
